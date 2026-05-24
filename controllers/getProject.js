9c46de49f1fcaef6d20a587dfb9c804a5ab236b4
annuprajapati3 <annuprajapati0004@gmail.com>
2026-05-24 09:30:16 +0530

    Initial commit for MERN Jira clone backend

--- controllers/getProject.js
+++ controllers/getProject.js
@@ -1,12 +1,29 @@
 const Project = require("../models/Project");
 
 const getProjects = async (req, res) => {
-    const projects = await Project.find()
 
-    res.status(200).json({
-        projects
-    });
+    try {
 
-}
+        const projects = await Project.find()
+
+            .populate("createdBy", "name email role")
+
+            .populate("members", "name email role")
+
+            .populate("organization", "name");
+
+        res.status(200).json({
+            success: true,
+            projects
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
 
 module.exports = getProjects;
